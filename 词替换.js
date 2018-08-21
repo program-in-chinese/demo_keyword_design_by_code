e9287d627baa97c4adbc96c601803111f469a283
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-08-21 12:32:07 -0700

    原型完成, 支持字典动态生成对照表并实时替换源码

--- 词替换.js
+++ 词替换.js
@@ -1,5 +1,3 @@
-// for in 对于 在
-// if 如果
 var 关键词词典 = {
   'for': '对于',
   'in': '在',
@@ -25,10 +23,38 @@ function 替换所有关键词() {
 
 }
 
-function 替换for(){
-  关键词词典['for'] = document.getElementsByName("for替换")[0].value;
-  替换所有关键词();
+// <div><span>for</span><input type="text" name="for替换" value="对于"></div>
+function 创建替换词条(原词文本, 替换文本) {
+  var 词条 = document.createElement("div");
+
+  var 原词 = document.createElement('span');
+  原词.textContent = 原词文本;
+
+  var 替换 = document.createElement('input');
+  var 替换输入框名 = 原词文本 + '替换';
+  替换.type = 'text';
+  替换.name = 替换输入框名;
+  替换.value = 替换文本;
+
+  词条.appendChild(原词);
+  词条.appendChild(替换);
+  return 词条;
+}
+
+function 取输入监听函数(关键词) {
+  return function() {
+    关键词词典[关键词] = document.getElementsByName(关键词 + '替换')[0].value;
+    替换所有关键词();
+  };
 }
 
 替换所有关键词();
-document.getElementsByName("for替换")[0].addEventListener('input', 替换for);
+
+var 替换表 = document.getElementById("替换表");
+for(var 关键词 in 关键词词典) {
+  替换表.appendChild(创建替换词条(关键词, 关键词词典[关键词]));
+
+  var 替换输入框名 = 关键词 + '替换';
+  document.getElementsByName(替换输入框名)[0].addEventListener('input', 取输入监听函数(关键词));
+
+}
